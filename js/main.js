291b3cc60c28d913525a2eda7ba0e3f69856ce3f
rem{e}koh <git@remekoh.me>
2016-05-31 20:03:25 -0500

    v1.5

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-$("#fb");
+$("");
 var mainText = "";
 for (var i = 1; i < 101; i++) {
 
 	mainText += "<li>";
 
-    if ((i % 3 === 0) && (i % 5 === 0)) {
+    if ((i % 3 == 0) && (i % 5 == 0)) {
 
         mainText += "FizzBuzz";
 
-    } else if(i % 3 === 0) {
+    } else if(i % 3 == 0) {
 
         mainText += "Fizz";
 
-    } else if(i % 5 === 0) {
+    } else if(i % 5 == 0) {
 
         mainText += "Buzz";
 
